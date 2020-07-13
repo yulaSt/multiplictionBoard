7052cb048ba44dce685dd6e779660ff30ad87b16
ranwahle <ran.wahle@gmail.com>
2020-07-13 23:43:56 +0300

    Player animation on recording

--- client/recorder.js
+++ client/recorder.js
@@ -58,7 +58,7 @@ export class RecordingComponent extends HTMLElement {
         let mediaRecorder;
         const microphoneButton = this.microphone;
         this.microphone.onclick =  microphoneButton.ontouchstart = microphoneButton.ontouchend =  async () => {
-            const permissionStatus = navigator.permissions && navigator.permissions.request.request ?
+            const permissionStatus = navigator.permissions && navigator.permissions.request ?
                 (await navigator.permissions.request({name: 'microphone'})) : {state: 'granted'};
             if (permissionStatus.state !== 'granted') {
                 return;
